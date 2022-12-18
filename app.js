97110ccdfab19f2d0b6457fc4a346992da1cfdcf
philip <philipbangura69@gmail.com>
2022-12-18 16:42:12 +0100

    JavaScript

---
+++ app.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const projectOneBtn = document.querySelector(".p1btn");
+const projectTwoBtn = document.querySelector(".p2btn");
+const projectThreeBtn = document.querySelector(".p3btn");
+const projectFourBtn = document.querySelector(".p4btn");
+
+projectOneBtn.addEventListener("click", () => {
+  alert(
+    "This Recipe app is under developement, please check in some other time"
+  );
+});
+
+projectTwoBtn.addEventListener("click", () => {
+  alert(
+    "This Recipe app is under developement, please check in some other time"
+  );
+});
+
+projectThreeBtn.addEventListener("click", () => {
+  alert(
+    "This Recipe app is under developement, please check in some other time"
+  );
+});
+
+projectFourBtn.addEventListener("click", () => {
+  alert(
+    "This Recipe app is under developement, please check in some other time"
+  );
+});
